826775c820b9769f8c36620ca613ba151d9f09c4
Rechafos <43388192+Rechafos@users.noreply.github.com>
2018-10-13 11:03:34 -0500

    Menu de pausa
    -Cuartos terminados
    -Texto en las entradas de las puertas
    -Base del menu de pausa

---
+++ Assets/Scripts/ShowTextInCollider.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowTextInCollider : MonoBehaviour {
+
+    public GameObject target;
+    public GameObject TextThatAppearsWhenInteracted;
+    int turnoDelDialogo = 0;
+   
+    void Start()
+    {
+        TextThatAppearsWhenInteracted.SetActive(false);
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        EnableText(other);
+    }
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        EnableText(other);
+
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.name == target.name)
+        {
+            TextThatAppearsWhenInteracted.SetActive(false);
+        }
+    }
+    private void EnableText(Collider2D other)
+    {
+        if (other.name == target.name)
+        {
+
+
+                TextThatAppearsWhenInteracted.SetActive(true);
+           
+        }
+    }
+}
