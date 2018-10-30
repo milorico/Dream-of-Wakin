47929e00cacb71b8e78c5b64d98b248410e863f6
milorico <35440649+milorico@users.noreply.github.com>
2018-10-30 07:23:31 -0500

    PuzzleMillerAgregado
    me toco arregarlo >:v

---
+++ Assets/Scripts/ChangeCamera.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCamera : MonoBehaviour {
+
+    public Camera cam1;
+    public Camera cam2;
+    void Start()
+    {
+        cam1.GetComponent<Camera>().enabled = true;
+        cam2.GetComponent<Camera>().enabled = false;
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown("c"))
+        {
+            if (cam1.GetComponent<Camera>().enabled == true)
+            {
+                cam1.GetComponent<Camera>().enabled = false;
+                cam2.GetComponent<Camera>().enabled = true;
+            }
+            else if (cam2.GetComponent<Camera>().enabled == true)
+            {
+                cam2.GetComponent<Camera>().enabled = false;
+                cam1.GetComponent<Camera>().enabled = true;
+            }
+        }
+    }
+}
