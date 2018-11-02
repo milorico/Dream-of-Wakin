f51a05efb1dddb0ef371d37a2d42093a829e63b8
jsilva463 <43388184+jsilva463@users.noreply.github.com>
2018-11-02 03:34:13 -0500

    Otros ajustes del nivel 1

--- Assets/Scripts/Texts/ScreenplayController.cs
+++ Assets/Scripts/Texts/ScreenplayController.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,18 +6,54 @@ public class ScreenplayController : MonoBehaviour {
 
 	//SP is a screenplay
 	//AS is a sound
-	public GameObject SPStep;
-	public GameObject ASFootStepsSound;
+	public GameObject NoteText;
+	public GameObject DeadText;
+	public GameObject Gun;
+	bool noteRead;
+	bool gunActive;
+
 	
 
 	// Use this for initialization
 	void Start () {
-		
+		NoteText.SetActive(false);
+		noteRead = false;
+		gunActive = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
 
+		if (noteRead){
+			NoteText.SetActive(true);
+		}
 	}
+
+	void OnCollisionExit2D ( Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Note"))
+        {
+            noteRead = true;
+        }
+    }
+
+     void OnCollisionStay2D (Collision2D other)
+    {
+    	if ((other.gameObject.CompareTag("Deceased"))&(Input.GetKeyDown(KeyCode.K)))
+        {
+            print("Tenemos arma pez");
+            gunActive = true;
+            Destroy(DeadText);
+            Destroy(Gun);
+        }
+
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("TextOnce"))
+        {
+            Destroy (other);
+        }
+    }
 }
